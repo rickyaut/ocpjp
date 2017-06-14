c52646274e114db27ee31b58090b4c3870d6cdfa
Ricky Liu <ricky.liu@isobar.com>
2017-06-14 16:53:27 +1000

    update

--- src/test/java/ocpjp/PracticeTestIV.java
+++ src/test/java/ocpjp/PracticeTestIV.java
@@ -1,7 +1,8 @@
 package ocpjp;
 
-import static java.lang.System.out;
 import static java.lang.System.err;
+
+import java.util.function.Predicate;
 
 import org.junit.Test;
 
@@ -9,6 +10,32 @@ public class PracticeTestIV {
 	/**
 	 * 
 	 */
+	@Test
+	public void question2(){
+		Predicate<String> p = (String s) ->s.startsWith("a");
+		err.println("QUESTION 2");
+	}
+
+	/**
+	 * 
+	 */
+	enum Month{
+		January(31),
+		Febuary(28){
+			public int getDays(int year){
+				return year%4 == 0 ? 29 : this.days;
+			}
+		};
+		
+		protected int days;
+		
+		private Month(int days){
+			this.days = days;
+		}
+		public int getDays(int year){
+			return days;
+		}
+	}
 	@Test
 	public void question(){
 		err.println("QUESTION XX");
